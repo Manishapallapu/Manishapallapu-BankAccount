5b42382448ebd935d9044209bfc9b5ede3b82510
Manishapallapu <manishapallapu123@gmail.com>
2023-10-07 11:57:29 +0530

    passed

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -8,7 +8,6 @@ public class BankService {
     /**
      * 
      * 
-     * 
      * The balance should be kept private. If it were public, then other developers could write code in other classes
      * that could cause the balance to exhibit unintended behavior, like being negative. The private access modifier
      * will allow only the methods within this class to interact with the balance.
